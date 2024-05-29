b2be4c2b9fd024c842ffde06efd19273ead7afd7
Zanxiang Wang <zanxiang.wang@outlook.com>
2024-05-28 22:18:17 -0500

    syntex

--- airView/app.py
+++ airView/app.py
@@ -3,28 +3,35 @@ import os
 from flask import Flask
 from flask_restful import Api
 from flask_jwt_extended import JWTManager
-from flask_mail import flask_mail
+from flask_mail import Mail
 from dotenv import load_dotenv
-# from authentication.auth_feature import UserRegistration, UserLogin, AdminRegistration
 
-app = Flask(__main__)
+# Load environment variables from .env file
+load_dotenv()
+
+# Initialize Flask app
+app = Flask(__name__)
+
+# Configure app from environment variables
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 app.config['JWT_SECRET_KEY'] = os.getenv('JWT_SECRET_KEY')
 app.config['MAIL_SERVER'] = os.getenv('MAIL_SERVER')
 app.config['MAIL_PORT'] = int(os.getenv('MAIL_PORT'))
-app.config['MAIL_USE_TLS'] = os.getenv('MAIL_USE_TLS')
+app.config['MAIL_USE_TLS'] = os.getenv('MAIL_USE_TLS') == 'True'
 app.config['MAIL_USERNAME'] = os.getenv('MAIL_USERNAME')
 app.config['MAIL_PASSWORD'] = os.getenv('MAIL_PASSWORD')
 app.config['MAIL_DEFAULT_SENDER'] = os.getenv('MAIL_DEFAULT_SENDER')
 
+# Initialize extensions
 api = Api(app)
 jwt = JWTManager(app)
 mail = Mail(app)
 
-# api.add_resources(UserRegistration, '/register')
-# api.add_resources(UserLogin, '/login')
-# api.add_resources(AdminREgistration, '/admin/register')
+# Add resource endpoints
+# api.add_resource(UserRegistration, '/register')
+# api.add_resource(UserLogin, '/login')
+# api.add_resource(AdminRegistration, '/admin/register')
 
+# Run the app
 if __name__ == '__main__':
     app.run(debug=True)
-
