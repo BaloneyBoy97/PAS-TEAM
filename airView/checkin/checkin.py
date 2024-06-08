178d94daa589671e609ff6e8ed664d636880b5f0
bhumulanandinireddy <bhumula.nandini1602@gmail.com>
2024-06-08 18:47:49 -0500

    checkin button code

--- airView/checkin/checkin.py
+++ airView/checkin/checkin.py
@@ -1,44 +1,14 @@
-import logging
-from flask import request, jsonify, make_response
+from flask import request, jsonify
 import sys
 import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from authentication.operation import get_flight_details, get_user_details, get_user_by_username,  get_booked_flights
 import sqlite3
+from database.data import curr, conn
 
-"""
-Define the directory path where you want to create the database.
-Create the database file in database directory.
-"""
-database_dir = os.path.join(os.path.dirname(__file__), '..', 'database')
-os.makedirs(database_dir, exist_ok=True)
-
-db_path = os.path.join(database_dir, 'appdata.db')
-
-conn = sqlite3.connect(db_path)
-curr = conn.cursor()
-
-@app.route('/checkin', methods=['POST'])
-def check_in():
-    data = request.get_json()
-    username = data.get('username')
-    print(username)
-    print("ihfdsasiuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuhhhhhhhhhhhhhhhh")
-    if username:
-        user = get_user_by_username(username)
-        if user:
-            user_id = user['userid']
-            curr.execute("UPDATE bookings SET is_checked_in = ? WHERE userid = ?", (1, user_id))
-            conn.commit()
-            flight_details = get_booked_flights(username)
-            if flight_details:
-                flight_id = flight_details['flightid']
-                flight = get_flight_details(flight_id)
-                user_details = get_user_details(user_id)
-                return jsonify({'flight_details': flight, 'user_details': user_details}), 200
-            else:
-                return jsonify({'message': 'No booked flights found for the user.'}), 404
-        else:
-            return jsonify({'message': 'User not found.'}), 404
+def check_in(user_id):
+    if user_id:
+        curr.execute("UPDATE bookings SET is_checked_in = ? WHERE userid = ?", (1, user_id))
+        conn.commit()
     else:
-        return jsonify({'message': 'Invalid request.'}), 400
+        return jsonify({'message': '.'}), 404
